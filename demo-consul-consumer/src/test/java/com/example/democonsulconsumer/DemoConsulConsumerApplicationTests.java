34015f86dfc9dfb87277cd872547b8dd801e8c5c
lilinj2000 <lilinj2000@gmail.com>
2020-03-16 12:12:28 +0800

    demo-consul-consumer be added.

---
+++ demo-consul-consumer/src/test/java/com/example/democonsulconsumer/DemoConsulConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democonsulconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConsulConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
