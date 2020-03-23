7a0b10498dcf570a38fc155291ad0b735d0ffdb9
lilinj2000 <lilinj2000@gmail.com>
2020-03-23 11:10:05 +0800

    user-service be done.

---
+++ demo-user-service/src/main/java/com/example/demouserservice/DemoUserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.demouserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DemoUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoUserServiceApplication.class, args);
+    }
+
+}
