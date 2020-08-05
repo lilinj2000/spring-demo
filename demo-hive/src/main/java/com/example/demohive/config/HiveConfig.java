f0cd1550fa43cb1f349749fbce7c3bbabfde9288
lilinj2000 <lilinj2000@gmail.com>
2020-08-05 11:18:00 +0800

    done.

--- demo-hive/src/main/java/com/example/demohive/config/HiveConfig.java
+++ demo-hive/src/main/java/com/example/demohive/config/HiveConfig.java
@@ -1,42 +1,45 @@
 package com.example.demohive.config;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+
 
 @Configuration
-@ConfigurationProperties(prefix = "spring.datasource.hive")
+
 public class HiveConfig {
-    private String url;
-    private String driverClassName;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getDriverClassName() {
-        return driverClassName;
-    }
-
-    public void setDriverClassName(String driverClassName) {
-        this.driverClassName = driverClassName;
-    }
+//    private String url;
+//    private String driverClassName;
+//
+//    public String getUrl() {
+//        return url;
+//    }
+//
+//    public void setUrl(String url) {
+//        this.url = url;
+//    }
+//
+//    public String getDriverClassName() {
+//        return driverClassName;
+//    }
+//
+//    public void setDriverClassName(String driverClassName) {
+//        this.driverClassName = driverClassName;
+//    }
 
     @Bean(name = "hiveDataSource")
     @Qualifier("hiveDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.hive")
     public DataSource dataSource() {
-//        DataSource dataSource = DataSourceBuilder.create().build();
-        DataSource dataSource = new DataSource();
-        dataSource.setUrl(url);
-        dataSource.setDriverClassName(driverClassName);
+        DataSource dataSource = DataSourceBuilder.create().build();
+//        DataSource dataSource = new DataSource();
+//        dataSource.setUrl(url);
+//        dataSource.setDriverClassName(driverClassName);
 
         return dataSource;
     }
