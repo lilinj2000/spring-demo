8990b4356ad94ec952fb3d79beb46f000c431c21
lilinj2000 <lilinj2000@gmail.com>
2020-08-25 15:19:48 +0800

    done.

---
+++ demo-hive/run-demohive.sh
@@ -0,0 +1,1 @@
+#nohup /usr/java/jdk1.8.0_181-cloudera/bin/java -jar -Djava.security.krb5.debug=true -Djava.security.krb5.conf="/etc/krb5.conf" -Djavax.security.auth.useSubjectCredsOnly=false -Dspring.profiles.active=test demo-hive-0.0.1-SNAPSHOT.jar >demo-hive.log 2>&1 &
